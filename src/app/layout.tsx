eccb183f3856a833bd57d77745e6e638973c7442
Z User <z@container>
2026-05-17 22:57:03 +0000

    Remove duplicate AutoBackup from layout (already in app-shell)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { AutoBackup } from '@/components/auto-backup';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,7 +45,6 @@ export default function RootLayout({
         >
           {children}
           <Toaster richColors position="top-right" />
-          <AutoBackup />
         </ThemeProvider>
       </body>
     </html>

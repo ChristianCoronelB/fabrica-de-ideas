5167e20ce1dada94e9c35f2d9280161b9c7a6ed9
Z User <z@container>
2026-05-18 00:20:32 +0000

    auto-backup

--- backups/database_dump.sql
+++ backups/database_dump.sql
@@ -1,5 +1,5 @@
 -- Fábrica de Ideas Database Dump
--- Generated at: 2026-05-18T00:19:22.375Z
+-- Generated at: 2026-05-18T00:20:32.830Z
 -- ============================================
 
 -- Table: AppSetting
@@ -12,8 +12,8 @@ CREATE TABLE "AppSetting" (
     "updatedAt" DATETIME NOT NULL
 );
 
-INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlh0000oiymv0qyiwtw', 'copyrightText', 'Fábrica de Ideas Ucacue', 1779063069077, 1779063520615);
-INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlj0001oiymkn8tnnc9', 'organizationName', 'Fábrica de Ideas', 1779063069079, 1779063520614);
+INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlh0000oiymv0qyiwtw', 'copyrightText', 'Fábrica de Ideas, Creado por Ing. Christian Coronel Balderramo', 1779063069077, 1779063577820);
+INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlj0001oiymkn8tnnc9', 'organizationName', 'Fábrica de Ideas FCEE', 1779063069079, 1779063577818);
 
 -- Table: Area
 DROP TABLE IF EXISTS "Area";

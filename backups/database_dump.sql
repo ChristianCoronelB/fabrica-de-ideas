695eee14ae68ee2ecac4de831cb8c6a89945e364
Z User <z@container>
2026-05-18 00:18:39 +0000

    auto-backup

--- backups/database_dump.sql
+++ backups/database_dump.sql
@@ -1,5 +1,5 @@
 -- Fábrica de Ideas Database Dump
--- Generated at: 2026-05-18T00:18:07.999Z
+-- Generated at: 2026-05-18T00:18:38.981Z
 -- ============================================
 
 -- Table: AppSetting
@@ -12,8 +12,8 @@ CREATE TABLE "AppSetting" (
     "updatedAt" DATETIME NOT NULL
 );
 
-INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlh0000oiymv0qyiwtw', 'copyrightText', 'Fábrica de Ideas', 1779063069077, 1779063482758);
-INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlj0001oiymkn8tnnc9', 'organizationName', 'Fábrica de Ideas:', 1779063069079, 1779063482757);
+INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlh0000oiymv0qyiwtw', 'copyrightText', 'Fábrica de Ideas Ucaue', 1779063069077, 1779063508543);
+INSERT INTO "AppSetting" ("id", "key", "value", "createdAt", "updatedAt") VALUES ('cmpag7tlj0001oiymkn8tnnc9', 'organizationName', 'Fábrica de Ideas', 1779063069079, 1779063508542);
 
 -- Table: Area
 DROP TABLE IF EXISTS "Area";
